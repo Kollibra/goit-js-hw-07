b362ec6a6e8217dd47771dce20243b6fa1903e1d
Iryna Kovalova <100217476+Kollibra@users.noreply.github.com>
2023-12-03 18:15:11 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,23 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+  const {
+    elements: { email, password } 
+  } = event.currentTarget;
+
+
+ if (email.value === "" || password.value === "") {
+   return alert('All form fields must be filled in') 
+   
+}
+
+ const userForm = { email: email.value, Password: password.value }
+    console.log(userForm)
+    event.currentTarget.reset()
+}   
+    
+    
